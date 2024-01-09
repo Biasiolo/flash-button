69c97afc9d3d2b04db9027869df015587e3c21c9
Biasiolo <biasiolorafael@gmail.com>
2024-01-08 22:55:55 -0300

    print

---
+++ screenshot.js
@@ -0,0 +1,36 @@
+// screenshot.js
+
+$(document).ready(function() {
+    $(".btn").on("click", function() {
+        html2canvas(document.body).then(function(canvas) {
+            var imageData = canvas.toDataURL("image/png");
+
+            var link = document.createElement('a');
+            link.href = imageData;
+            link.download = 'screenshot.png';
+
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+
+            // Exibe a mensagem de sucesso
+            showSuccessMessage();
+        });
+    });
+
+    function showSuccessMessage() {
+        // Cria um elemento para a mensagem de sucesso
+        var successMessage = document.createElement('div');
+        successMessage.className = 'success-message';
+        successMessage.innerHTML = 'Screenshot salvo com sucesso!';
+
+        // Adiciona a mensagem ao corpo do documento
+        document.body.appendChild(successMessage);
+
+        // Remove a mensagem após alguns segundos (opcional)
+        setTimeout(function() {
+            document.body.removeChild(successMessage);
+        }, 3000); // Remove a mensagem após 3 segundos (ajuste conforme necessário)
+    }
+});
